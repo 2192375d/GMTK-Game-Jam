bcb83665878955883e6618c1082d43a4d98c3349
Kido <63456333+the-kido@users.noreply.github.com>
2026-07-23 21:04:55 -0400

    feat: Fixed outlines so they work again yay

--- scripts/game/interactable.gd
+++ scripts/game/interactable.gd
@@ -16,15 +16,27 @@ var player_can_interact: bool = false
 
 func _ready():
 	outline = visual_mesh.duplicate() as MeshInstance3D
-	outline.material_override = mat
+	outline.material_override = mat.duplicate()
 	add_child(outline)
-	outline.visible = false
-	pass
+	(outline.material_override as BaseMaterial3D).albedo_color = Color.TRANSPARENT
 	
 func toggle_outline(toggle: bool):
 	player_can_interact = toggle
-	
+	print(player_can_interact)
 
+func _process(_delta: float) -> void:
+	var outline_color: Color
+	
+	if player_can_interact:
+		outline_color = Color.WHITE
+	elif within_photo_area:
+		outline_color = Color.RED
+	else:
+		outline_color = Color.TRANSPARENT
+	
+	(outline.material_override as BaseMaterial3D).albedo_color = outline_color
+	
+	
 @abstract
 func on_e_interact(player: Player) -> Signal
 
