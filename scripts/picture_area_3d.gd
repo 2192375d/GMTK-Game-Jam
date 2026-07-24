026c07e35de35c337e02b25081817d1b98df9a24
Krish Patel <63456333+the-kido@users.noreply.github.com>
2026-07-24 09:30:39 -0400

    feat: Fixed bug where object held by player wouldn't be detected

--- scripts/picture_area_3d.gd
+++ scripts/picture_area_3d.gd
@@ -3,8 +3,21 @@ extends Area3D
 
 @export var capture_shape: CollisionShape3D
 
-
 func get_capture_size() -> Vector2:
 	var box_shape: BoxShape3D = capture_shape.shape as BoxShape3D
 	var area_scale: Vector3 = global_transform.basis.get_scale()
 	return Vector2(box_shape.size.x * absf(area_scale.x), box_shape.size.z * absf(area_scale.z))
+
+
+# Useful for the level to know if ur safe or not
+var in_risk_items: Array[Interactable]
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is Interactable:
+		body.within_photo_area = true
+		in_risk_items.append(body)
+
+func _on_body_exited(body: Node3D) -> void:
+	if body is Interactable:
+		body.within_photo_area = false
+		in_risk_items.erase(body)
--- scripts/player/held_item_controller.gd
+++ scripts/player/held_item_controller.gd
@@ -2,20 +2,24 @@ class_name HeldItemController
 extends Node3D
 
 var held_item: Interactable
+var held_item_layer: int
 
 func pick_up_item(interactable: Interactable) -> void:
 	print("picking up item", interactable.name)
 	held_item = interactable
+	held_item_layer = held_item.collision_layer
 	
 	interactable.freeze = true
-	interactable.collision_shape.disabled = true
+	#interactable.collision_shape.disabled = true
+	interactable.collision_layer = 16
 
 func drop_item() -> void:
 	print("attemptiong to drop...")
 	if held_item:
 		print("dropping item", held_item.name)
 		held_item.freeze = false
-		held_item.collision_shape.disabled = false
+		#held_item.collision_shape.disabled = false
+		held_item.collision_layer = held_item_layer
 		held_item = null
 
 func _process(_delta: float) -> void:
