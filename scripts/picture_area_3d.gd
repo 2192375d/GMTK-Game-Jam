fcd1a39d2f0f3fa8f47197e4b67a5875b28ab901
Krish Patel <63456333+the-kido@users.noreply.github.com>
2026-07-25 15:37:21 -0400

    feat: Added even more objects. Also added tripod which affects where the camera area goes!

--- scripts/picture_area_3d.gd
+++ scripts/picture_area_3d.gd
@@ -2,12 +2,15 @@ class_name PictureArea3D
 extends Area3D
 
 @export var capture_shape: CollisionShape3D
+@export var tripod: RigidBody3D
 
 func get_capture_size() -> Vector2:
 	var box_shape: BoxShape3D = capture_shape.shape as BoxShape3D
 	var area_scale: Vector3 = global_transform.basis.get_scale()
 	return Vector2(box_shape.size.x * absf(area_scale.x), box_shape.size.z * absf(area_scale.z))
 
+func _process(_delta: float) -> void:
+	global_position.x = tripod.global_position.x
 
 # Useful for the level to know if ur safe or not
 var in_risk_items: Array[Interactable]
