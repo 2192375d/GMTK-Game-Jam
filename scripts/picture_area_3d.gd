4ba2d55b9628a3964523e528659c244431ee7c60
Kido <63456333+the-kido@users.noreply.github.com>
2026-07-25 16:34:08 -0400

    feat: Added "dots" shader instead of images and it looks banging

--- scripts/picture_area_3d.gd
+++ scripts/picture_area_3d.gd
@@ -4,6 +4,10 @@ extends Area3D
 @export var capture_shape: CollisionShape3D
 @export var tripod: RigidBody3D
 
+# Useful for the level to know if ur safe or not
+var in_risk_items: Array[Interactable]
+var is_player_within: bool
+
 func get_capture_size() -> Vector2:
 	var box_shape: BoxShape3D = capture_shape.shape as BoxShape3D
 	var area_scale: Vector3 = global_transform.basis.get_scale()
@@ -11,10 +15,11 @@ func get_capture_size() -> Vector2:
 
 func _process(_delta: float) -> void:
 	global_position.x = tripod.global_position.x
+	
+	# Update dotted colour if anything is wrong
+	var dotted_color = Color.RED if not is_player_within or in_risk_items.size() > 0 else Color.GREEN
+	(($Dots.mesh as PlaneMesh).material as ShaderMaterial).set_shader_parameter("line_color", dotted_color)
 
-# Useful for the level to know if ur safe or not
-var in_risk_items: Array[Interactable]
-var is_player_within: bool
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Interactable:
@@ -22,7 +27,7 @@ func _on_body_entered(body: Node3D) -> void:
 		in_risk_items.append(body)
 	if body is Player:
 		is_player_within = true
-		
+
 func _on_body_exited(body: Node3D) -> void:
 	if body is Interactable:
 		body.within_photo_area = false
